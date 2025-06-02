dd5aff0d1ed8c28d8c476c63f64fc8a907bf1d86
TheDarkLady <lakshmichaithanya13@gmail.com>
2025-06-02 18:13:21 +0530

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import './App.css'
-import About from './components/About'
-import CalculatorDashboard from './components/CalculatorDashboard'
-import ErrorPage from './components/ErrorPage'
-import ExchangeRates from './components/ExchangeRates'
-import Header from './components/Header'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import CalculatorDashboard from "./components/CalculatorDashboard";
+import ErrorPage from "./components/ErrorPage";
+import ExchangeRates from "./components/ExchangeRates";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-    <Header />
-    <CalculatorDashboard/>
-    <ExchangeRates/>
-    <About/>
-    <ErrorPage/>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<CalculatorDashboard />} />
+          <Route path="/exchange-rates" element={<ExchangeRates />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/error-page" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,124 @@
-import React from 'react'
+  import React from 'react';
+  import {
+    AppBar,
+    Box,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText,
+    Toolbar,
+    Typography,
+    Button
+  } from '@mui/material';
+  import MenuIcon from '@mui/icons-material/Menu';
+  import { useNavigate } from 'react-router-dom';
 
-const Header : React.FC = () => {
-  return (
-    <div>
-        <h1>Header Component</h1>
-      
-    </div>
-  )
-}
+  interface Props {
+    window?: () => Window;
+  }
 
-export default Header
+  const drawerWidth = 240;
+  const navItems = ['Home', 'Exchange Rates (live)', 'About', 'Error Page'];
+
+  const Header: React.FC<Props> = (props) => {
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const navigate = useNavigate();
+
+    const handleDrawerToggle = () => {
+      setMobileOpen((prevState) => !prevState);
+    };
+
+    const handleNavigate = (id:number, item:string) => {
+      if(item === "Home"){
+        navigate("/")
+      }
+      else if(item === "Exchange Rates (live)"){
+        navigate("/exchange-rates")
+      }
+      else if(item === "About"){
+        navigate("/about")
+      }
+      else if(item === "Error Page"){
+        navigate("/error-page")
+      }
+    }
+
+    const drawer = (
+      <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+        <Typography variant="h6" sx={{ my: 2 }}>
+          MUI
+        </Typography>
+        <Divider />
+        <List>
+          {navItems.map((item) => (
+            <ListItem key={item} disablePadding>
+              <ListItemButton sx={{ textAlign: 'center' }}>
+                <ListItemText primary={item} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+      <>
+        <AppBar component="nav" position="static" sx={{ backgroundColor: '#1976d2' }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              Loan Calculator
+            </Typography>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              {navItems.map((item, index) => (
+                <Button key={index} sx={{ color: '#fff' }} onClick={() => handleNavigate(index, item)}>
+                  {item}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+
+        <nav>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': {
+                boxSizing: 'border-box',
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </nav>
+      </>
+    );
+  };
+
+  export default Header;
